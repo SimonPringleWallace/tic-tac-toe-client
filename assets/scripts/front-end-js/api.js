baaf982ec24fdbd297b6e0e4e6d64ca5641736f3
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-08-09 16:50:12 -0400

    Swap out apiURL for Url in 'api.js'
    Trade out the individual urls in 'api.js' for config.apiUrl to make sure
    that the url will change automatically depending on the server it
    is hosted on.

--- assets/scripts/front-end-js/api.js
+++ assets/scripts/front-end-js/api.js
@@ -3,7 +3,7 @@ const store = require('../store.js')
 const config = require('../config.js')
 const signUp = function (data) {
   return $.ajax({
-    url: 'https://tic-tac-toe-wdi.herokuapp.com/' + 'sign-up',
+    url: config.apiUrl + '/sign-up',
     method: 'POST',
     header: 'Content-Type: application/json',
     data
@@ -20,7 +20,7 @@ const signIn = function (data) {
 
 const signOut = function () {
   return $.ajax({
-    url: 'https://tic-tac-toe-wdi.herokuapp.com/' + 'sign-out',
+    url: config.apiUrl + '/sign-out',
     method: 'DELETE',
     headers: {
       'Authorization': 'Token token=' + store.user.token
@@ -32,7 +32,7 @@ const changePW = function (data) {
   console.log(data)
   console.log(store.user.token)
   return $.ajax({
-    url: 'https://tic-tac-toe-wdi.herokuapp.com/' + 'change-password',
+    url: config.apiUrl + '/change-password',
     method: 'PATCH',
     data,
     headers: {
